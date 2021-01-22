2afb5e83d6051fcc8b9454c0046991e7ea1371da
Jason Nyland <jason@jasonnyland.com>
2021-01-22 10:12:38 -0800

    restored Task class to match old project

--- src/main/java/com/hcl/model/Task.java
+++ src/main/java/com/hcl/model/Task.java
@@ -1,10 +1,14 @@
 package com.hcl.model;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.Data;
 
@@ -15,5 +19,11 @@ public class Task {
 	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
 	private long id;
 	private String name;
-	private boolean done;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date startDate;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date endDate;
+	private String description;
+	private String email;
+	private int severity;
 }
